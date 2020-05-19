f9c1974efeead5c80acd83c6e45be71058f26422
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-05-19 15:19:29 +0530

    setted up folder and started the server installed expres and nodemon

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+const port = 8000;
+// ROutes
+
+
+// how do we start listening to the server
+app.listen(port, function(err){
+    if(err){
+        console.log(`Error in running the server: ${err}`);
+    }
+    console.log(`Server started on port ${port}`);
+})
