c6c4459ffc26f0c061a29d07f1d6e3ea3abe55b3
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-05-19 16:27:31 +0530

    writtedn function for get request when i want to see whold data

--- routes/posts.js
+++ routes/posts.js
@@ -3,10 +3,17 @@ const express = require('express');
 const router = express.Router();
 const Post = require('../models/Post');
 
-router.get('/', (req, res) => {
-    res.send('We are on posts');
+// Get back all the posts
+router.get('/', async (req, res) => {
+    try {
+    const posts = await Post.find();   
+    res.json(posts);
+    } catch (err) {
+        res.json({message: err});
+    }
 });
 
+// submit a post
 router.post('/', async (req, res) => {
    const post = new Post({
     title: req.body.title,
@@ -20,5 +27,6 @@ router.post('/', async (req, res) => {
    }
    
 });
+
 
 module.exports = router;
