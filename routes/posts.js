8b5225977cc37a9771c0a477ed218a5a37801569
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-05-19 16:22:37 +0530

    reduced code size by using async and await

--- routes/posts.js
+++ routes/posts.js
@@ -7,19 +7,18 @@ router.get('/', (req, res) => {
     res.send('We are on posts');
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
    const post = new Post({
     title: req.body.title,
     description: req.body.description
    });
-   post.save()
+   try{
+    const savedPost = await post.save()
+    res.json(savedPost);
+   }catch(err){
+    res.json({message: err});
+   }
    
-   .then(data => {
-       res.json(data);
-   })
-   .catch(err => {
-       res.json({message: err});
-   })
 });
 
 module.exports = router;
